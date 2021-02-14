cfc246eee83172a1cc8f7d8f062487c2067e15f1
DARK_Mechanic <62394670+Dark-Mechanic@users.noreply.github.com>
2021-02-14 08:20:21 +0000

    Update ip.py

--- ip.py
+++ ip.py
@@ -1,3 +1,10 @@
+#############################################
+#Copyright of D A R K _ Mechanic , 2021     #
+#https://twitter.com/dark_mechanic          #
+#https://www.youtube.com/Dark_mechanic/     #
+#############################################
+
+
 import socket as s
 host = input("Enter the website: ")
 print(f'IP of {host} is {s.gethostbyname(host)}')
